f53e7e21c57df18007018ef40432f915c5ea7ed4
dawei126156 <dawei126156@163.com>
2019-09-04 08:36:44 +0800

    login

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import "./index.css";
 import RouterView from './router/RouterView';
 import router from './router/router';
 import {BrowserRouter} from 'react-router-dom';
+import "./index.css";
+import 'antd/dist/antd.css';
+
+
+import {Provider} from 'mobx-react';
+import store from './store'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <RouterView routes={router.routes} />
-  </BrowserRouter>,
-  document.getElementById("root") as HTMLElement
+  <Provider {...store}>
+    <BrowserRouter> 
+      <RouterView routes={router.routes}/>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root') as HTMLElement
 );
